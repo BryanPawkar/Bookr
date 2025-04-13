a4af4fcdc95d5b1ba6018a49c84489bfab65fbc2
BRACO <bracosmo@gmail.com>
2025-04-13 10:59:31 -0400

    Exercise 2

--- reviews/views.py
+++ reviews/views.py
@@ -4,5 +4,7 @@ from django.shortcuts import render
 def index(request):
     name = "World"
     return render(request, "base.html", {"name": name})
-
+def book_search(request):
+    search_text = request.GET.get("search", "")
+    return render(request, "search-results.html", {"search_text": search_text})
 # Create your views here.
